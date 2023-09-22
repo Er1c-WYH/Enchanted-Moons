17ff26d871c8a6b8839d0c6c0a845fc4aeb0ac89
MGT <warriorstorm15@gmail.com>
2023-09-22 17:03:21 +0300

    Mint Moon Update

---
+++ Items/MusicBoxes/MintMoonMusicBox.cs
@@ -0,0 +1,21 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BlueMoon.Items.MusicBoxes
+{
+    public class MintMoonMusicBox : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.CanGetPrefixes[Type] = false;
+            ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.MusicBox;
+
+            MusicLoader.AddMusicBox(Mod, MusicLoader.GetMusicSlot(Mod, "Music/MintMoon"), ModContent.ItemType<MintMoonMusicBox>(), ModContent.TileType<MintMoonMusicBoxTile>());
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToMusicBox(ModContent.TileType<MintMoonMusicBoxTile>(), 0);
+        }
+    }
+}
